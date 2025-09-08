5986af3f110b26866ae768bb9776f135ab480fff
DekyDeky <dekyifprtb@gmail.com>
2025-09-08 00:08:17 -0300

    feat: adição de um componente teste

--- frontend/src/components/Tarefa/Interessados.jsx
+++ frontend/src/components/Tarefa/Interessados.jsx
@@ -1,55 +1,59 @@
 import './Interessados.scss'
 import PHIcon from '../../assets/ph.jpg'
 
-export default function Interessados(){
+export default function Interessados({dadosAluno}){
     return (
         <div className="Interessados">
             <section className="Interessados-secao">
-                <h2 className='Interessados-titulo Interessados-titulo'>Nome Usuário</h2>
-                <h3 className='Interessados-area Interessados-texto'>Área de Atuação do Aluno</h3>
-                <h3 className='Interessados-instituicao Interessados-texto'>Instituição Ligada</h3>
+                <h2 className='Interessados-titulo Interessados-titulo'>{dadosAluno.nome}</h2>
+                <h3 className='Interessados-area Interessados-texto'>{dadosAluno.area}</h3>
+                <h3 className='Interessados-instituicao Interessados-texto'>{dadosAluno.instituicao}</h3>
 
                 <div className="Interessados-info-habilidades interessados-sessao">
                     <h4 className='Interessados-titulo Interessados-texto'>Habilidades</h4>
                     <div className="Interessados-info-item Interessados-habilidade">
-                        <span className='Interessados-texto Interessados-generalItem'>Design Gráfico</span>
-                        <span className='Interessados-texto Interessados-generalItem'>Programação Web</span>
-                        <span className='Interessados-texto Interessados-generalItem'>Canva</span>
+                        {
+                            dadosAluno.habilidades.map((item, idx) => (
+                                <span className='Interessados-texto Interessados-generalItem' key={idx}>{item}</span>
+                            ))
+                        }
                     </div>
                 </div>
 
                 <div className='Interessados-info-sobre interessados-sessao'>
                     <h4 className='Interessados-tituloSobre Interessados-titulo Interessados-texto'>Sobre Mim</h4>
-                    <p className='Interessados-sobreTarefa Interessados-texto'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras condimentum sem a risus gravida facilisis. Vivamus id mauris suscipit, elementum lorem eget, pulvinar libero. Mauris lobortis eleifend volutpat. Duis commodo vel odio id pharetra. Sed rhoncus pharetra nisl, eget fermentum elit ornare sed. In maximus sodales vulputate. </p>
+                    <p className='Interessados-sobreTarefa Interessados-texto'>{dadosAluno.sobre}</p>
                 </div>
 
         
                 <div className='Interessados-info interessados-sessao'>
 
                     <table className="Interessados-info-formacao">
-                        <tr>
+                        <thead>
+          
+                            <tr>
 
-                            <th className='Interessados-titulo Interessados-texto'>Formação</th>
-                            <th className='Interessados-titulo Interessados-texto'>Conclusão</th>
+                                <th className='Interessados-titulo Interessados-texto'>Formação</th>
+                                <th className='Interessados-titulo Interessados-texto'>Conclusão</th>
 
-                        </tr>
-                        <tr>
+                            </tr>
+                        </thead>
+                        <tbody>
 
-                            <td className="Interessados-info-item Interessados-generalItem">
-                                <div className="Interessados-formacao-curso"> Informática para Internet </div>
-                                <div className="Interessados-formacao-instituicao">IFPR, Campus Telêmaco Borba</div>
-                            </td>
-                            <td className='Interessados-info-item Interessados-generalItem'>xxxx</td>                     
-                            
+                                {
+                                    dadosAluno.formacoes.map((formacao, idx) => (
+                                        <tr key={idx}>
+                                            <td className="Interessados-info-item Interessados-generalItem Interessados-curso">
+                                                <div className="Interessados-formacao-curso fw-bold"> {formacao.nome} </div>
+                                                <div className="Interessados-formacao-instituicao">{formacao.instituicao}</div>
+                                            </td>
+                                            <td className='Interessados-info-item Interessados-generalItem Interessados-conclusao'>{formacao.conclusao}</td>
+                                        </tr>
+                                    ))
+                                }
 
-                        </tr>
-                        <tr>
-                            <td className="Interessados-info-item Interessados-generalItem">
-                                <div className="Interessados-formacao-curso"> Técnico em Análise e Desenvolvimento de Sistemas </div>
-                                <div className="Interessados-formacao-instituicao">IFPR, Campus Telêmaco Borba</div>
-                            </td>
-                            <td className='Interessados-info-item Interessados-generalItem'>xxxx</td>      
-                        </tr>
+ 
+                        </tbody>
 
                     </table>
 
--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -7,7 +7,34 @@ import './homePage.scss'
 
 export default function HomePage(){
 
-    
+    const HabilidadesAlunoTeste = [
+        "Design Gráfico",
+        "Programação Web",
+        "Canva"
+    ]
+
+    const Formacao1 = {
+        nome: "Informática para Internet",
+        instituicao: "IFPR, Campus Telêmaco Borba",
+        conclusao: 2023
+    }
+
+    const Formacao2 = {
+        nome: "Análise e Desenvolvimento de Sistemas",
+        instituicao: "IFPR, Campus Telêmaco Borba",
+        conclusao: 2026
+    }
+
+    const Formacoes = [Formacao1, Formacao2]
+
+    const AlunoTeste = {
+        nome: "Jonathan da Silva",
+        area: "Programação Web",
+        instituicao: "IFPR, Campus Telêmaco Borba",
+        habilidades: HabilidadesAlunoTeste,
+        sobre: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras condimentum sem a risus gravida facilisis. Vivamus id mauris suscipit, elementum lorem eget, pulvinar libero. Mauris lobortis eleifend volutpat. Duis commodo vel odio id pharetra. Sed rhoncus pharetra nisl, eget fermentum elit ornare sed. In maximus sodales vulputate. ",
+        formacoes: Formacoes
+    }
 
     return(
         <main>
@@ -16,7 +43,7 @@ export default function HomePage(){
             <h1 className="home_titulo">Oportunidades</h1>
 
             <section className="home_tarefas">
-                <Interessado/>
+                <Interessado dadosAluno={AlunoTeste}/>
             </section>
         </main>
     )
