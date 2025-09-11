03e96ff7a071cd79fd7065cf1c4ddf5d26fb7a03
DekyDeky <dekyifprtb@gmail.com>
2025-09-11 02:18:23 -0300

    feat: tarefaAberta genérica

--- frontend/src/components/Tarefa/tarefaAberta.jsx
+++ frontend/src/components/Tarefa/tarefaAberta.jsx
@@ -4,7 +4,7 @@ import './tarefaAberta.scss'
 import PHIcon from '../../assets/ph.jpg'
 import TarefaInfo from './tarefaInfo'
 
-export default function TarefaAberta({ linkTarefa, filtro="", filtroTarefa="", statusBool=false}){
+export default function TarefaAberta({ linkTarefa, filtro="", filtroTarefa="", empresaInfo, tarefaInfo, statusBool=false}){
 
     let strFiltro = ""
 
@@ -26,26 +26,34 @@ export default function TarefaAberta({ linkTarefa, filtro="", filtroTarefa="", s
             break;
     }
 
+    const prazo = tarefaInfo.prazoInicio + " até " + tarefaInfo.prazoFim;
+    let quantidade;
+    if(tarefaInfo.remunerado){
+        quantidade = "R$ " + tarefaInfo.quantidade;
+    }else {
+        quantidade = "Não remunerado"
+    }
+
     return (
         <div className={ filtro == filtroTarefa ? "tarefaAberta" : "d-none"}>
             <section className="tarefaAberta-secao1">
-                <img src={PHIcon} alt="" width="100em"/>
+                <img src={empresaInfo.fotoUser} alt="" width="100em"/>
                 <h2 className='tarefaAberta-habilidadesTitulo tarefaAberta-titulo'>Habilidades</h2>
                 <div className='tarefaAberta-habilidades'>
-                    <h3 className='tarefaAberta-habilidade'>Design Gráfico</h3>
-                    <h3 className='tarefaAberta-habilidade'>Canva</h3>
-                    <h3 className='tarefaAberta-habilidade'>Programação Web</h3>
+                    {tarefaInfo.habilidades.map((item, idx) => (
+                        <h3 className='tarefaAberta-habilidade'>{item}</h3>
+                    ))}
                 </div>
             </section>
 
             <section className="tarefaAberta-secao2">
-                <h2 className='tarefaAberta-titulo tarefaAberta-titulo'>Título da Tarefa</h2>
-                <h3 className='tarefaAberta-empresa tarefaAberta-texto'>Nome da empresa</h3>
+                <h2 className='tarefaAberta-titulo tarefaAberta-titulo'>{tarefaInfo.titulo}</h2>
+                <h3 className='tarefaAberta-empresa tarefaAberta-texto'>{empresaInfo.nome}</h3>
                 <h3 className='tarefaAberta-tituloSobre tarefaAberta-titulo tarefaAberta-texto'>Sobre a Tarefa</h3>
-                <p className='tarefaAberta-sobreTarefa tarefaAberta-texto'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras condimentum sem a risus gravida facilisis. Vivamus id mauris suscipit, elementum lorem eget, pulvinar libero. Mauris lobortis eleifend volutpat. Duis commodo vel odio id pharetra. Sed rhoncus pharetra nisl, eget fermentum elit ornare sed. In maximus sodales vulputate. </p>
+                <p className='tarefaAberta-sobreTarefa tarefaAberta-texto'>{tarefaInfo.sobre}</p>
                 <div className='tarefaAberta-info'>
-                    <TarefaInfo titulo="Prazo:" conteudo="xx/05/2025 até xx/06/2025"/>
-                    <TarefaInfo titulo="Quantidade:" conteudo="R$ 100,00"/>
+                    <TarefaInfo titulo="Prazo:" conteudo={prazo}/>
+                    <TarefaInfo titulo="Quantidade:" conteudo={quantidade}/>
                     { statusBool && <TarefaInfo titulo="Status:" conteudo={strFiltro}/> }
                 </div>
                 <Link to={linkTarefa} className='tarefaAberta-btn'>Ver Mais</Link>
--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -7,6 +7,38 @@ import './homePage.scss'
 
 export default function HomePage(){
 
+    const empresaTeste = {
+        fotoUser: 'src/storage/logoEmpresaRandom.webp',
+        quantEstrelas: 4,
+        nome: "WebCom LTDA",
+        sobre: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras condimentum sem a risus gravida facilisis. Vivamus id mauris suscipit, elementum lorem eget, pulvinar libero. Mauris lobortis eleifend volutpat. Duis commodo vel odio id pharetra. Sed rhoncus pharetra nisl, eget fermentum elit ornare sed. In maximus sodales vulputate. ",
+
+    }
+
+    const tarefaTesteHabilidades = ["Design Gráfico", "Desenvolvimento Web", "Programação Web"]
+    const tarefaTesteHabilidades2 = ["Excel", "Logística", "Gestão"]
+
+    const tarefaTeste = {
+        titulo: "Reestruturar do site",
+        sobre: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras condimentum sem a risus gravida facilisis. Vivamus id mauris suscipit, elementum lorem eget, pulvinar libero. Mauris lobortis eleifend volutpat. Duis commodo vel odio id pharetra. Sed rhoncus pharetra nisl, eget fermentum elit ornare sed. In maximus sodales vulputate. ",
+        habilidades: tarefaTesteHabilidades,
+        prazoInicio: "xx/05/2025",
+        prazoFim: "xx/06/2025",
+        remunerado: true,
+        quantidade: 100.00,
+        pagamento: "PIX",
+        entrega: "através da plataforma"
+    }
+
+    const tarefaTeste2 = {
+        titulo: "Organizar planilhas de Entrega",
+        sobre: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras condimentum sem a risus gravida facilisis. Vivamus id mauris suscipit, elementum lorem eget, pulvinar libero. Mauris lobortis eleifend volutpat. Duis commodo vel odio id pharetra. Sed rhoncus pharetra nisl, eget fermentum elit ornare sed. In maximus sodales vulputate. ",
+        habilidades: tarefaTesteHabilidades2,
+        prazoInicio: "xx/05/2025",
+        prazoFim: "xx/06/2025",
+        remunerado: false,
+        entrega: "através da plataforma"
+    }
     
 
     return(
@@ -16,7 +48,8 @@ export default function HomePage(){
             <h1 className="home_titulo">Oportunidades</h1>
 
             <section className="home_tarefas">
-                <Tarefa linkTarefa="/tarefa"/>
+                <Tarefa linkTarefa="/tarefa?id=1" empresaInfo={empresaTeste} tarefaInfo={tarefaTeste}/>
+                <Tarefa linkTarefa="/tarefa?id=2" empresaInfo={empresaTeste} tarefaInfo={tarefaTeste2}/>
             </section>
         </main>
     )
